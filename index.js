65b43b4758a1504e87da06d680c6c1d5bbaa6536
nijat ibrahimov <ibrahimovofficial@gmail.com>
2021-08-06 15:45:11 +0400

    _slider00 v 1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector('.slider');
 const container = document.querySelector('.slider__container');
 const slides = document.querySelectorAll('.slide');
-const navigations = document.querySelectorAll('.navigation');
+const navigations = document.querySelectorAll('.slider__nav');
 
 let activeOrder = 0;
 
@@ -12,6 +12,11 @@ function init() {
     const slide = slides[i];
     slide.dataset.order = i;
     slide.style.transform = 'translate(-50%, -50%)'
+    slide.addEventListener('click', clickHandler);
+  }
+
+  for (const navigation of navigations) {
+    navigation.addEventListener('click', navigationHandler)
   }
 
   activeOrder = Math.floor(slides.length / 2);
@@ -21,17 +26,16 @@ function init() {
 
 function update() {
   const { width, height } = container.getBoundingClientRect();
-
-
+  const slideRect = slides[0].getBoundingClientRect();
   const a = width / 2;
   const b = height / 2;
   const delta = Math.PI / slides.length / 4;
 
   for (i = 0; i < slides.length; i++) {
     const leftSlide = document.querySelector(`.slide[data-order='${activeOrder - i}']`);
-
     if (leftSlide) {
       leftSlide.style.zIndex = slides.length - i;
+      leftSlide.style.opacity = 1 - (2 * i) / slides.length;
       leftSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 - delta * i * 2)}px`;
       leftSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 - delta * i * 2)}px`;
     }
@@ -39,8 +43,38 @@ function update() {
     const rightSlide = document.querySelector(`.slide[data-order='${activeOrder + i}']`);
     if (rightSlide) {
       rightSlide.style.zIndex = slides.length - i;
+      rightSlide.style.opacity = 1 - (2 * i) / slides.length;
       rightSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 + delta * i * 2)}px`;
       rightSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 + delta * i * 2)}px`;
     }
   }
+}
+
+function clickHandler() {
+  const order = parseInt(this.dataset.order, 10);
+  activeOrder = order;
+  update();
+}
+
+function navigationHandler(e) {
+  e.preventDefault();
+  const { dir } = this.dataset;
+  if (dir === 'next') {
+    console.log(slides.length);
+    if (activeOrder === Number(slides.length - 1)) {
+      activeOrder = 0;
+    } else {
+      activeOrder = Math.min(slides.length - 1, activeOrder + 1);
+    }
+  } else if (dir === 'prev') {
+    if (activeOrder === 0) {
+      activeOrder = Number(slides.length - 1);
+    } else {
+      console.log(slides.length - 1);
+      console.log(slides.length);
+      console.log(activeOrder);
+      activeOrder = Math.max(0, activeOrder - 1);
+    }
+  }
+  update();
 }
